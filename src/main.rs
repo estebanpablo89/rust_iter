31d5c2fb1b2bb8fb1f74b592cb6364aee2204db8
Esteban <estebanpablo89@gmail.com>
2025-02-26 15:48:02 -0500

    iter functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+fn print_elements1(elements: &Vec<String>){
+    for element in elements {
+        println!("{}", element);
+    }
+}
+
+fn print_elements2(elements: &Vec<String>){
+    elements.iter().for_each(|el|println!("{}", el));
+}
+
+fn print_elements3(elements: &Vec<String>){
+    elements
+        .iter()
+        .map(|el|format!("{} {}", el, el))
+        .for_each(|el|println!("{}", el));
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![String::from("red"), String::from("green"), String::from("blue")];
+
+    print_elements3(&colors);
+
+    // print_elements1(&colors);
+
+    // let mut colors_iter = colors.iter();
+
+    // println!("{:?}", colors_iter.next());
+    // println!("{:?}", colors_iter.next());
+    // println!("{:?}", colors_iter.next());
+    // println!("{:?}", colors_iter.next());
 }
